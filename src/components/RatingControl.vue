187bdac4eeb5a4469a7af6d3fb8151733257e40f
Yaroslav <dombrovskiy91@gmail.com>
2020-11-28 20:03:38 +0200

    build custom Control Component

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <li :class="{active: acitiveOption === 'poor'}"><button type="button" @click="active('poor')">Poor</button></li>
+    <li :class="{active: acitiveOption === 'average'}"><button type="button" @click="active('average')">Average</button></li>
+    <li :class="{active: acitiveOption === 'great'}"><button type="button" @click="active('great')">Great</button></li>
+  </ul>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        acitiveOption: null
+      }
+    },
+    methods: {
+      active(option) {
+        this.acitiveOption = option;
+        console.log(this.acitiveOption)
+      },
+    },
+  }
+</script>
+<style scoped>
+.active {
+  border-color:tomato;
+}
+.acitive button {
+  color: tomato;
+}
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  font: inherit;
+  border:none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -83,6 +83,10 @@
         <label for="how-other">Other</label>
       </div>
     </div>
+
+    <div class="form-control">
+      <rating-control></rating-control>
+    </div>
     <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm"/>
       <label for="confirm-terms">Agree to terms of use?</label>
@@ -94,7 +98,13 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue'
+
 export default {
+  
+  components: {
+    RatingControl,
+  },
   data() {
     return {
       userName: "",
